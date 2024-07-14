dc66da0f0cdbeb9da094a273f79d9247c91897d2
вашÐOleksandr0130 <olek1995sta@gmail.com>
2024-07-14 18:26:52 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 // Lessons Import
 import Lesson6 from "./lessons/Lesson6/Lesson6";
 import Lesson7 from "./lessons/Lesson7/Lesson7";
+import Lesson8 from "./lessons/Lesson8/Lesson8";
 
 // HomeWorks import
 import HomeWork7 from "./homeworks/HomeWork7/HomeWork7";
@@ -13,7 +14,8 @@ function App() {
   return <div className="App">
     {/* <Lesson6 /> */}
     {/* <Lesson7 /> */}
-    <HomeWork7 />
+    {/* <HomeWork7 /> */}
+    <Lesson8 />
   </div>;
 }
 
