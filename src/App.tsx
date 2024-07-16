47c7dfe6cbb73fce1fa8b6f94ee6b573c3980c3c
вашÐOleksandr0130 <olek1995sta@gmail.com>
2024-07-16 18:41:08 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Lesson8 from "./lessons/Lesson8/Lesson8";
 
 // HomeWorks import
 import HomeWork7 from "./homeworks/HomeWork7/HomeWork7";
+import Lesson09 from "./lessons/Lesson9/Lesson9";
+import Homework08 from "./homeworks/HomeWork8/HomeWork8";
 
 
 
@@ -15,7 +17,9 @@ function App() {
     {/* <Lesson6 /> */}
     {/* <Lesson7 /> */}
     {/* <HomeWork7 /> */}
-    <Lesson8 />
+    {/* <Lesson8 /> */}
+    {/* <Homework08 /> */}
+    <Lesson09 />
   </div>;
 }
 
