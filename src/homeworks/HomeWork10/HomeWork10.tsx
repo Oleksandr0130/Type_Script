f9e6aa55692ba5de73948524ada9c258b71f301d
вашÐOleksandr0130 <olek1995sta@gmail.com>
2024-07-17 18:56:48 +0200

    new HomeWork10

--- src/homeworks/HomeWork10/HomeWork10.tsx
+++ src/homeworks/HomeWork10/HomeWork10.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import Input from './Input';
-import Button from './Button';
-import './styles.css';
+import { useState } from "react";
+import Input from "./Input";
+import Button from "./Button";
+import "./styles.css";
+import { Homework10Component, Output1, Output2 } from "./styles";
 
 const Homework10 = () => {
-  const [input1, setInput1] = useState<string>('');
-  const [input2, setInput2] = useState<string>('');
-  const [output1, setOutput1] = useState<string>('');
-  const [output2, setOutput2] = useState<string>('');
+  const [input1, setInput1] = useState<string>("");
+  const [input2, setInput2] = useState<string>("");
+  const [output1, setOutput1] = useState<string>("");
+  const [output2, setOutput2] = useState<string>("");
 
   const handleButtonClick = () => {
     setOutput1(input1);
@@ -15,19 +16,23 @@ const Homework10 = () => {
   };
 
   return (
-    <div className="homework10-container">
-      <Input label="Input 1" value={input1} onChange={(e) => setInput1(e.target.value)} />
-      <Input label="Input 2" value={input2} onChange={(e) => setInput2(e.target.value)} />
-      <Button onClick={handleButtonClick}>Show Values</Button>
-      <div className="outputs">
-        <div className="output-block">
-          {output1}
-        </div>
-        <div className="output-block">
-          {output2}
-        </div>
+    <Homework10Component>
+      <div className="homework10-container">
+        <Input
+          label="Input 1"
+          value={input1}
+          onChange={(e) => setInput1(e.target.value)}
+        />
+        <Input
+          label="Input 2"
+          value={input2}
+          onChange={(e) => setInput2(e.target.value)}
+        />
+        <Button onClick={handleButtonClick}>Show Values</Button>
+        <Output1>{output1}</Output1>
+        <Output2>{output2}</Output2>
       </div>
-    </div>
+    </Homework10Component>
   );
 };
 
---
+++ src/homeworks/HomeWork10/styles.ts
@@ -0,0 +1,27 @@
+import styled from "@emotion/styled";
+
+export const Homework10Component = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  gap: 30px;
+`
+
+export const Output1 = styled.div`
+margin-top: 20px;
+  border: 1px solid black;
+  padding: 10px;
+  margin-bottom: 10px;
+  width: 200px;
+  text-align: center;
+`
+export const Output2 = styled.div`
+margin-top: 20px;
+  border: 1px solid black;
+  padding: 10px;
+  margin-bottom: 10px;
+  width: 200px;
+  text-align: center;
+`
