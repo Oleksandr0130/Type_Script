fab1d63c2908c2afdd6791d09ffa14488eb364cc
вашÐOleksandr0130 <olek1995sta@gmail.com>
2024-07-18 22:00:30 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import GlobalStyles from './components/styles/GlobalStyles';
+import GlobalStyles from "./components/styles/GlobalStyles";
 
-//Lessons import 
+//Lessons import
 // import Lesson06 from './lessons/Lesson06/Lesson06';
 // import Lesson07 from './lessons/Lesson07/Lesson07';
 // import Lesson08 from './lessons/Lesson08/Lesson08';
 // import Lesson09 from './lessons/Lesson09/Lesson09';
 // import Lesson10 from './lessons/Lesson10/Lesson10';
-import Lesson11 from './lessons/Lesson11/Lesson11';
+// import Lesson11 from './lessons/Lesson11/Lesson11';
+import Lesson12 from "./lessons/Lesson12/Lesson12";
 
-
-//Homeworks import 
+//Homeworks import
 // import Homework07 from './homeworks/Homework07/Homework07';
 // import Homework08 from './homeworks/Homework08/Homework08';
 // import Homework09 from './homeworks/Homework09/Homework09';
 // import Homework10 from './homeworks/HomeWork10/HomeWork10';
-
+// import Homework11 from "./homeworks/HomeWork11/HomeWork11";
 
 function App() {
   return (
@@ -29,7 +29,9 @@ function App() {
       {/* <Homework09/> */}
       {/* <Lesson10 /> */}
       {/* <Homework10 /> */}
-      <Lesson11 />
+      {/* <Lesson11 /> */}
+      {/* <Homework11 /> */}
+      <Lesson12 />
     </>
   );
 }
