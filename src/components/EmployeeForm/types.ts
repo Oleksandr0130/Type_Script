8c190756cf405af439552fa80194ccc33bf82b45
вашÐOleksandr0130 <olek1995sta@gmail.com>
2024-07-23 18:52:32 +0200

    new

---
+++ src/components/EmployeeForm/types.ts
@@ -0,0 +1,12 @@
+export interface EmployeeFormValue {
+    name: string
+    surname: string
+    jobTitle: string
+  }
+  
+  export enum EMPLOYEE_FORM_FIELDS {
+    NAME = 'name',
+    SURNAME = 'surname',
+    JOB_TITLE = 'jobTitle'
+  }
+  
